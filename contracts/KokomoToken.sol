e0e441cf1d328b3d83128434d219c448932d5f56
lichkingvamvoo <lich_king@vamvoo.io>
2021-05-04 15:06:02 +0900

    Change symbol to KOKOMO

--- contracts/KokomoToken.sol
+++ contracts/KokomoToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // KokomoToken with Governance.
-contract KokomoToken is BEP20('Kokomo Token', 'KOKO') {
+contract KokomoToken is BEP20('Kokomo Token', 'KOKOMO') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
